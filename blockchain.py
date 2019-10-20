b4beead13493eda44bc369b1eae21357cb08844d
Gon Lubel <greengon1991@gmail.com>
2019-10-20 11:58:37 +0300

    4_14 video

--- blockchain.py
+++ blockchain.py
@@ -7,11 +7,24 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Gon'
+participants = {'Gon'}
 
 
 def hash_block(block):
     return '-'.join([str(block[key]) for key in block])
 
+def get_balance(participant):
+    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+    amount_sent = 0
+    for tx in tx_sender:
+        if len(tx) > 0:
+            amount_sent += tx[0]
+    tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
+    amount_received = 0
+    for tx in tx_recipient:
+        if len(tx) > 0:
+            amount_received += tx[0]
+    return amount_received - amount_sent
 
 def get_last_blockchain_value():
     """ Returns the last value of the current blockchain """
@@ -33,6 +46,8 @@ def add_transaction(recipient, sender=owner, amount=1.0):
                    'amount': amount
                    }
     open_transactions.append(transaction)
+    participants.add(sender)
+    participants.add(recipient)
 
 
 def mine_block():
@@ -45,6 +60,7 @@ def mine_block():
         'transactions': open_transactions
     }
     blockchain.append(block)
+    return True
 
 
 def get_transaction_value():
@@ -87,6 +103,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output participants')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -97,9 +114,12 @@ while waiting_for_input:
         add_transaction(recipient, amount=amount)
         print(open_transactions)
     elif user_choice == '2':
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
         # Make sure that you don't try to "hack" the blockchain if it's empty
         if len(blockchain) >= 1:
@@ -120,5 +140,6 @@ while waiting_for_input:
         print_blockchain_elements()
         print('Invalid blockchain!')
         break
+    print(get_balance('Gon'))
 
 print('Done')
